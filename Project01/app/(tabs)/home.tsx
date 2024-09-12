2cd0741a7dcab70b688d289a299de028fdb7af2f
Kevinlzs <kevinlzs.04@gmail.com>
2024-09-11 19:30:07 -0700

    Added search bar design

--- Project01/app/(tabs)/home.tsx
+++ Project01/app/(tabs)/home.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, View, Text, StyleSheet, FlatList} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import {images} from '../../constants'
+import SearchInput from '../../components/SearchInput'
+
 
 const Home = () => {
   return (
-    <View >
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id:1 }]}
+        keyExtractor={(item) => item.id.toString() }
+        renderItem={({item}) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className='font-pmedium text-sm text-gray-100'>
+                  Welcome Back
+                </Text>
+                <Text className='text-2xl font-psemibold text-white'>
+                  Pokemon App
+                </Text>
+              </View>
+
+              <View className='mt-1.5'>
+                <Image
+                  source={images.logo}
+                  className='w-14 h-10'
+                  // resizeMode="contain"
+                />
+                <View></View>
+              </View>
+            </View>
+
+            <SearchInput/>
+          </View>
+
+        )}
+      />
+
+    </SafeAreaView>
+
   );
 };
 
